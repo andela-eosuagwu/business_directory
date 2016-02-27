a22f74d2c7b447f56e675b9f637af58010234497
emeka <emekaosuagwuandela@gmail.com>
2016-02-27 01:30:30 +0100

    add user controller

--- app/User.php
+++ app/User.php
@@ -24,7 +24,10 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = [
+		'full_name', 
+		'email', 
+		'password'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
