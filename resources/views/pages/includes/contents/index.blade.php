ec0752bfa17cb675737882cf73754c5de6be6b2e
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-02-29 22:57:47 +0100

    add partners page

--- resources/views/pages/includes/contents/index.blade.php
+++ resources/views/pages/includes/contents/index.blade.php
@@ -463,19 +463,7 @@
 				</section>
 				<!--end Our Team-->
 				<!--Partners-->
-				<section id="partners" class="block">
-					<div class="container">
-						<header><h2>Partners</h2></header>
-						<div class="logos">
-							<div class="logo"><a href="#"><img src="assets/img/logo-partner-01.png" alt=""></a></div>
-							<div class="logo"><a href="#"><img src="assets/img/logo-partner-02.png" alt=""></a></div>
-							<div class="logo"><a href="#"><img src="assets/img/logo-partner-03.png" alt=""></a></div>
-							<div class="logo"><a href="#"><img src="assets/img/logo-partner-04.png" alt=""></a></div>
-							<div class="logo"><a href="#"><img src="assets/img/logo-partner-05.png" alt=""></a></div>
-						</div>
-					</div>
-					<!--/.container-->
-				</section>
+				@include('pages.includes.sections.partners')
 				<!--end Partners-->
 			</div>
 			<!-- end Page Content-->
