c40c861931ebb999c96e9dc4c726f74260d1f569
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-02-29 22:50:41 +0100

    add index page

--- resources/views/pages/includes/sections/link.blade.php
+++ resources/views/pages/includes/sections/link.blade.php
@@ -10,3 +10,15 @@
 
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
 
+
+
+<link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/bootstrap-select.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/jquery.nouislider.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/leaflet.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/colors/blue.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/user.style.css" type="text/css">
