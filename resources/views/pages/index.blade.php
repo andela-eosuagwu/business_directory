665c59a7f3f14297e7258e6c6fb151550a8e2233
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-02-29 22:55:46 +0100

    add index recent

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -25,19 +25,7 @@
 		</div>
 	</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-	<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
 <script type="text/javascript" src="assets/js/before.load.js"></script>
 <script type="text/javascript" src="assets/js/leaflet.js"></script>
 <script type="text/javascript" src="assets/js/leaflet-providers.js"></script>
