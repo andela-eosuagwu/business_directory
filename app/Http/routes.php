89120e7494c5afe57ac7b69a3b86dd3180d64bf6
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-02 19:22:52 +0100

    update routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,90 @@
 <?php
-function load_asset($asset_url)
-{
-    return ( env('APP_ENV') === 'production' ) ? secure_asset($asset_url) : asset($asset_url);
-}
-function domain_name()
-{
-    return Request::root();
-}
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+	return view('pages.index.index');
+});
+
+Route::get('/home', function () {
+	return view('pages.index.index');
+});
+
+
+
+
+
+
+Route::get('/login', function () {
+	return view('pages.login');
+});
+
+Route::post('login', [
+	'uses' => 'Auth\AuthController@login', 
+	'as' => 'register'
+]);
+
+Route::get('/register', function () {
+	return view('pages.register');
+});
+
+Route::post('register', [
+	'uses' => 'Auth\AuthController@register', 
+	'as' => 'register'
+]);
+
+
+Route::get('users', [
+	'uses' => 'UserController@index', 
+	'as' => 'register'
+]);
+
+Route::group(['prefix' => 'companies'], function () {
+
+	Route::get('/', [
+		'uses' 	=> 'CompanyController@index', 
+		'as' 	=> 'companies'
+	]);
+
+});
+
+Route::group(['prefix' => 'company'], function () {
+	
+	Route::get('/', function ()    {
+		return view('pages.company_detail');
+	});	
+
+	Route::get('create', function ()    {
+		return view('pages.company.create');
+	});	
+
+	Route::post('create', [
+		'uses' 	=> 'CompanyController@create', 
+		'as' 	=> 'company.create'
+	]);
+
+	Route::get('{id}', [
+		'uses' 	=> 'CompanyController@view', 
+		'as' 	=> 'company.{id}'
+	]);
+
+	Route::get('{id}/edit', [
+		'uses' 	=> 'CompanyController@edit', 
+		'as' 	=> 'company.{id}.edit'
+	]);
+
+	Route::get('search/{keyword}', [
+		'uses' 	=> 'CompanyController@search', 
+		'as' 	=> 'company.search.{keyword}'
+	]);
+
+});
