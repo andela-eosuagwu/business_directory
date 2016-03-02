7d5a325da17862d0b3024e0b0b3397004d3e9e4b
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-02 19:20:46 +0100

    add helper file

--- resources/views/pages/index/index.blade.php
+++ resources/views/pages/index/index.blade.php
@@ -1,17 +1,18 @@
 <html>
 	<head>
+	{{secure_asset()}}
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/jquery.nouislider.min.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/colors/green.css" type="text/css">
-		<link rel="stylesheet" href="assets/css/user.style.css" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/bootstrap/css/bootstrap.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/bootstrap-select.min.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/owl.carousel.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/jquery.mCustomScrollbar.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/jquery.nouislider.min.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/colors/green.css') !!}}" type="text/css">
+		<link rel="stylesheet" href="{{!! asset('assets/css/user.style.css') !!}}" type="text/css">
 
 		<title>Spotter - Universal Directory Listing HTML Template</title>
 
