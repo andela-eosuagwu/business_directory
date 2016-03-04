2c596a77048b04ef3a2273b6a00e9eed166bc668
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-04 23:52:31 +0100

    update resouse

--- resources/views/pages/user/edit.blade.php
+++ resources/views/pages/user/edit.blade.php
@@ -1,7 +1,16 @@
 @extends('master')
 
 @section('title', 'Register')
-
+	<link href="assets/fonts/font-awesome.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/bootstrap-select.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/jquery.mCustomScrollbar.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/jquery.nouislider.min.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/colors/green.css" type="text/css">
+	<link rel="stylesheet" href="assets/css/user.style.css" type="text/css">
+	
 @section('content')
 
 <body onunload="" class="page-subpage page-item-detail navigation-off-canvas" id="page-top">
