806268ead619562703f316bb4cbf158151cf9517
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-04 16:15:06 +0100

    update reviews migration

--- database/migrations/2016_03_04_150749_create_reviews_table.php
+++ database/migrations/2016_03_04_150749_create_reviews_table.php
@@ -14,7 +14,20 @@ class CreateReviewsTable extends Migration
 	{
 		Schema::create('reviews', function (Blueprint $table) {
 			$table->increments('id');
+			$table->string('name');
+			$table->string('email');
+			$table->text('message');
+			
+			$table->integer('company_id')->unsigned();
+			
 			$table->timestamps();
+
+
+
+			 $table->foreign('company_id')
+				->references('id')
+				->on('companies');
+
 		});
 	}
 
