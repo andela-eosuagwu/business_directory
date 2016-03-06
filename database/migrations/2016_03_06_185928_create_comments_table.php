558fb687cea62035b6f4bfb0090a68cbe5751598
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-06 20:05:07 +0100

    update comment migration

--- database/migrations/2016_03_06_185928_create_comments_table.php
+++ database/migrations/2016_03_06_185928_create_comments_table.php
@@ -5,26 +5,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCommentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('email');
+			$table->string('name');
+			$table->string('message');
+			$table->integer('blog_id')->unsigned();
+			$table->timestamps();
+		
+			$table->foreign('blog_id')
+				->references('id')
+				->on('blogs');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('comments');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('comments');
+	}
 }
