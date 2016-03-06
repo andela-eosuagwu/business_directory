2611aba65df606a791b5c1bea12bcf0736c4964b
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-03-06 16:21:21 +0100

    add blog model

---
+++ app/Model/Blog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\Company;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+	protected $fillable = [
+		'title',
+		'image',
+		'article',
+		'tags',
+	];
+
+}
+
